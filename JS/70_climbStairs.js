23c70154486aab9cc91f040bf0b95335cd62aa56
LHKong7 <jamesklh5666@gmail.com>
2021-11-16 23:35:17 +0800

    update progress of 11/16

---
+++ JS/70_climbStairs.js
@@ -0,0 +1,38 @@
+/**
+ * @charset: UTF-8
+ * @Date:
+ * @Author: Linghan Kong
+ * @Algoset:
+ * @Memo: Giving up is not an option
+ */
+
+/******************************************************************
+ *
+ * Problem Description:
+ *
+ *
+ * TestCase:
+ * Output:
+ * Explanation:
+ * *****************************************************************/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {
+    let p = 0, q = 0, r = 1;
+    for (let i = 1; i <= n; ++i) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+};
+
+/**
+ * @Idea:
+ * @Solution:
+ * @DataStructure:
+ */
+
