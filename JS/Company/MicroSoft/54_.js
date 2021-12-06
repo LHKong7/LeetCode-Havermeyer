2d6dd6c8a5e81123aacada8c8a41c80df9552292
LHKong7 <jamesklh5666@gmail.com>
2021-12-07 01:28:30 +0800

    update progress of 12/6

---
+++ JS/Company/MicroSoft/54_.js
@@ -0,0 +1,28 @@
+/**
+ * @charset: UTF-8
+ * @Date:
+ * @Author: Linghan Kong
+ * @Algoset:
+ * @Memo: Giving up is not an option
+ */
+
+/******************************************************************
+ *
+ * Problem Description:
+ *
+ *
+ * TestCase:
+ * Output:
+ * Explanation:
+ * *****************************************************************/
+
+
+
+
+
+/**
+ * @Idea:
+ * @Solution:
+ * @DataStructure:
+ */
+
