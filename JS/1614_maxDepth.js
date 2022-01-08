9ecad8ffc3c8aa8551a03b2cc16cbb20a3aa2823
LHKong7 <jamesklh5666@gmail.com>
2022-01-08 23:35:48 +0800

    update progress of 1/7

---
+++ JS/1614_maxDepth.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var maxDepth = function(s) {
+    let ans = 0, size = 0;
+    for (let i = 0; i < s.length; ++i) {
+        const ch = s[i];
+        if (ch === '(') {
+            ++size;
+            ans = Math.max(ans, size);
+        } else if (ch === ')') {
+            --size;
+        }
+    }
+    return ans;
+};
---
+++ JS/219_containsNearbyDuplicate.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const mySet = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (mySet.has(nums[i])) {
+            return true;
+        }
+        
+        mySet.add(nums[i]);
+
+        if (mySet.size > k) {
+            mySet.delete(nums[i-k]);
+        }
+    }
+    return false;
+};
