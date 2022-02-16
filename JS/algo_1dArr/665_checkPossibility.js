3344922d020a665d4eba834a28c3336354be58a8
LHKong7 <jamesklh5666@gmail.com>
2022-02-16 22:51:36 +0800

    reformat folder

--- JS/algo_1dArr/665_checkPossibility.js
+++ JS/algo_1dArr/665_checkPossibility.js
@@ -9,7 +9,7 @@ var checkPossibility = function(nums) {
         if (nums[i] < nums[i-1]) {
             count += 1;
             if (count > 1) return false;
-            if (i - 2 >= 0 && nums[i] < nums[i-2]) {
+            if (nums[i] < nums[i-2]) {
                 nums[i] = nums[i - 1];
             } else {
                 nums[i - 1] = nums[i];
