6e61ec60db49d5ad92849def4ae67dd445739102
LHKong7 <jamesklh5666@gmail.com>
2022-03-19 23:42:54 +0800

    update progress of 3/19

---
+++ JS/algo_tree/basic.js
@@ -0,0 +1,9 @@
+// basic File about tree written in JavaScript
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val);
+  this.left = (left === undefined ? null : left);
+  this.right = (right === undefined ? null : right);
+};
+
+
+
