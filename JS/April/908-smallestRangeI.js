126dbaa8f929fdee2b4d812a482b8de8848b4c84
LHKong7 <jamesklh5666@gmail.com>
2022-04-30 23:36:21 +0800

    update progress of 4/30

---
+++ JS/April/908-smallestRangeI.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeI = function(nums, k) {
+    const minNum = _.min(nums);
+    const maxNum = _.max(nums);
+    return maxNum - minNum <= 2 * k ? 0 : maxNum - minNum - 2 * k;
+};
