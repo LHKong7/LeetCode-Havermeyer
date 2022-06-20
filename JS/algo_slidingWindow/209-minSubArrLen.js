b60649a0e74519bafc97966e03fafcebe0fb6e5b
LHKong7 <jamesklh5666@gmail.com>
2022-06-20 23:56:02 +0800

    update progress of 6/20

--- JS/algo_slidingWindow/209-minSubArrLen.js
+++ JS/algo_slidingWindow/209-minSubArrLen.js
@@ -3,21 +3,58 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(target, nums) {
+ var minSubArrayLen = function(target, nums) {
     let leftPtr = 0, rightPtr = 0;
-    let currWindowSum = 0;
-    let res = Number.MAX_SAFE_INTEGER;
+    const n = nums.length;
+    let res = Number.MAX_SAFE_INTEGER, windowSum = 0;
 
-    while (rightPtr < nums.length) {
-        currWindowSum += nums[rightPtr];
+    while (rightPtr < n) {
+        windowSum += nums[rightPtr];
 
-        while (currWindowSum >= target) {
-            res = Math.min(rightPtr-leftPtr+1, res);
-            currWindowSum -= nums[leftPtr];
+        while (windowSum >= target) {
+            res = Math.min(res, rightPtr - leftPtr + 1);
+            windowSum -= nums[leftPtr];
             leftPtr++;
         }
-
         rightPtr++;
+    }
+
+    return res === Number.MAX_SAFE_INTEGER ? 0 : res;
+};
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+
+    const firstGETargetEl = (nums, target) => {
+        if (nums === null || nums.length === 0) return -1;
+    
+        let left = 0; right = nums.length - 1;
+        while (left <= right) {
+            let mid = left + Math.floor((right - left) / 2);
+    
+            if (target <= nums[mid]) {
+                if (mid === 0 || nums[mid-1] < target) return mid;
+                else right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    
+    let prefixSum = new Array(nums.length + 1).fill(0);
+
+    for (let i = 1; i <= nums.length; i++) prefixSum[i] = prefixSum[i-1] + nums[i-1];
+
+    let res = Number.MAX_SAFE_INTEGER;
+    for (let i = 1; i < prefixSum.length; i++) {
+        let t = target + prefixSum[i-1];
+        let j = firstGETargetEl(prefixSum, t);
+        if (j < 0) continue;
+        if (j <= nums.length) res = Math.min(res, j - i + 1);
     }
 
     return res === Number.MAX_SAFE_INTEGER ? 0 : res;
