522d6c99522bec0b5708055dc6e1192292047950
LHKong7 <jamesklh5666@gmail.com>
2022-06-29 23:46:31 +0800

    update progress of 6/29

---
+++ JS/algo_ApplicationI/349-intersection.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersection = function(nums1, nums2) {
+    if (nums1 === null && nums2 === null) return [];
+
+    let res = [];
+    let hashStore = new Set();
+
+    for (let num1 of nums1) {
+        hashStore.add(num1);
+    }
+
+    for (let num2 of nums2) {
+        if (hashStore.has(num2)) {
+            res.push(num2);
+            hashStore.delete(num2)
+        }
+    }
+
+    return res;
+};
+
+var intersection = function(nums1, nums2) {
+    nums1.sort((x, y) => x - y);
+    nums2.sort((x, y) => x - y);
+    const length1 = nums1.length, length2 = nums2.length;
+    let index1 = 0, index2 = 0;
+    const intersection = [];
+    while (index1 < length1 && index2 < length2) {
+        const num1 = nums1[index1], num2 = nums2[index2];
+        if (num1 === num2) {
+            // 保证加入元素的唯一性
+            if (!intersection.length || num1 !== intersection[intersection.length - 1]) {
+                intersection.push(num1);
+            }
+            index1++;
+            index2++;
+        } else if (num1 < num2) {
+            index1++;
+        } else {
+            index2++;
+        }
+    }
+    return intersection;
+};
