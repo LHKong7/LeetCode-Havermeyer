79b6c806d9891200c59a25c4a1d66eb58a11446c
LHKong7 <jamesklh5666@gmail.com>
2022-07-06 23:55:49 +0800

    update progress of 7/6

--- HTML-CSS/canvas_learning/JS/main.js
+++ HTML-CSS/canvas_learning/JS/main.js
@@ -1,9 +1,17 @@
-var canvasEle = document.getElementById('myCanvas')
-
-if (canvasEle && canvasEle.getContext) {
-    var context = canvasEle.getContext('2d');
-
-    if (context) {
-        context.fillRect(0, 0, 150, 100);
-    }
+const canvasEle = document.getElementById('myCanvas');
+const ctx = canvasEle.getContext('2d');
+const drawStraightLine = (ctx) => { // from (25, 25) to (125, 125)
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(125, 125);
+    ctx.closePath();
+    ctx.stroke();
 }
+drawStraightLine(ctx);
+
+canvasEle.addEventListener('click', function (e) {
+    const canvasInfo = canvasEle.getBoundingClientRect()
+    console.log("canvasInfo: ", canvasInfo);
+    console.log("isPointInPath: ", ctx.isPointInPath(e.clientX - canvasInfo.left, e.clientY - canvasInfo.top))
+})
+
