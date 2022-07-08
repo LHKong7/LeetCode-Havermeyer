3cbaaa74414a585f3438a4069715067262dc53cf
LHKong7 <jamesklh5666@gmail.com>
2022-07-08 23:54:43 +0800

    update progress of 7/8

---
+++ JS/June/minCostToMoveChips.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} position
+ * @return {number}
+ */
+ var minCostToMoveChips = function(position) {
+    let even = 0, odd = 0;
+    for (const pos of position) {
+        if ((pos & 1) !== 0) {
+            odd++;
+        } else {
+            even++;
+        }
+    }
+    return Math.min(odd, even);
+};
+
