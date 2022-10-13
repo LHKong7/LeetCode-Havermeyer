2f033a1859c507602fa1697ae456d9d2c6a97cb1
LHKong7 <jamesklh5666@gmail.com>
2022-10-13 22:20:10 +0800

    update progress of 10/13

---
+++ JS/algo_Oct/769-maxChunksToSorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var maxChunksToSorted = function(arr) {
+    let m = 0, res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        m = Math.max(m, arr[i]);
+        if (m === i) {
+            res++;
+        }
+    }
+    return res;
+};
