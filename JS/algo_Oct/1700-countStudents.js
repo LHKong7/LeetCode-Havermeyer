ba267dddc5d6bf62fb77b056b32093fc1d9dbc7b
LHKong7 <jamesklh5666@gmail.com>
2022-10-19 21:37:36 +0800

    update progress of 10/19

---
+++ JS/algo_Oct/1700-countStudents.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+ var countStudents = function(students, sandwiches) {
+    let s1 = 0, s0 = 0;
+    for (let stu of students) {
+        s1 += stu;
+    }
+    s0 = students.length - s1;
+
+    for (let i = 0; i < sandwiches.length; i++) {
+        if (sandwiches[i] === 0 && s0 > 0) {
+            s0--;
+        } else if (sandwiches[i] === 1 && s1 > 0) {
+            s1--;
+        } else {
+            break;
+        }
+    }
+
+    return s0 + s1;
+};
