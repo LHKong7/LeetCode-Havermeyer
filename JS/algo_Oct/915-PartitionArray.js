71f3a5dcb80e3e0e9f9b26de02c0f364a653ac1e
LHKong7 <jamesklh5666@gmail.com>
2022-10-24 23:51:19 +0800

    update progress of 10/24

---
+++ JS/algo_Oct/915-PartitionArray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var partitionDisjoint = function(nums) {
+    const n = nums.length;
+    let leftMax = nums[0], leftPos = 0, curMax = nums[0];
+    for (let i = 1; i < n - 1; i++) {
+        curMax = Math.max(curMax, nums[i]);
+        if (nums[i] < leftMax) {
+            leftMax = curMax;
+            leftPos = i;
+        }
+    }
+    return leftPos + 1;
+};
