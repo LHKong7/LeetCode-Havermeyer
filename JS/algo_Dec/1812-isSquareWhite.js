5f6154b53ccd9e2218ea7cad467e11c814a87724
LHKong7 <jamesklh5666@gmail.com>
2022-12-08 23:33:08 +0800

    update progress of 12/8

---
+++ JS/algo_Dec/1812-isSquareWhite.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+ var squareIsWhite = function(coordinates) {
+    return ((coordinates[0].charCodeAt() - 'a'.charCodeAt() + 1) + (coordinates[1].charCodeAt() - '0'.charCodeAt())) % 2 === 1;
+};
