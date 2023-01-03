c54288aea5087d798b9fb8dda407d2cd1fc1f41a
LHKong7 <jamesklh5666@gmail.com>
2023-01-03 23:30:03 +0800

    2023: 1/3 update leetcode daily problem

---
+++ JS/algo_Jan/2042_areNumbersAscending.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function(s) {
+    let prev = 0, pos = 0;
+
+    while (pos < s.length) {
+        if (isDigit(s[pos])) {
+            let curr = 0;
+
+            while (pos < s.length && isDigit(s[pos])) {
+                curr = curr * 10 + s[pos].charCodeAt() - '0'.charCodeAt(); 
+                pos++;
+            }
+
+            if (curr <= prev) {
+                return false;
+            }
+            prev = curr;
+        } else {
+            pos++;
+        }
+    }
+
+    return true;
+};
+
+var isDigit = (char) => {
+    return parseInt(char) || parseInt(char) === 0;
+}
