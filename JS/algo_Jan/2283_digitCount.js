55618c029173f4d146ba7df7e9abf351be327eb0
LHKong7 <jamesklh5666@gmail.com>
2023-01-11 23:23:48 +0800

    2023: 1/11 update leetcode daily problem

---
+++ JS/algo_Jan/2283_digitCount.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var digitCount = function(num) {
+    const h = new Map();
+    const n = num.length;
+    for (let i = 0; i < n; i++) {
+        const digit = num[i].charCodeAt() - '0'.charCodeAt();
+        h.set(digit, (h.get(digit) || 0) + 1);
+    }
+    for (let i = 0; i < n; i++) {
+        const v = num[i].charCodeAt() - '0'.charCodeAt();
+        if ((h.get(i) || 0) !== v) {
+            return false;
+        }
+    }
+    return true;
+};
